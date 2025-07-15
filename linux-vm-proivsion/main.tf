68322c6527d33510f660d629a358e8fb91db5346
NAGI REDDY KONDA REDDY <61970671+nagiredk@users.noreply.github.com>
2025-07-15 15:48:49 +0530

    Update main.tf

--- linux-vm-proivsion/main.tf
+++ linux-vm-proivsion/main.tf
@@ -1,1 +1,20 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
