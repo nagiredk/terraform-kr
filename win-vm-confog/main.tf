b3719f7a708dcd255404309bb237a42a4da03064
NAGI REDDY KONDA REDDY <61970671+nagiredk@users.noreply.github.com>
2025-07-15 14:20:33 +0530

    Update main.tf

--- win-vm-confog/main.tf
+++ win-vm-confog/main.tf
@@ -18,10 +18,10 @@ provider "azurerm" {
   use_msi = false
   
   // Your existing configuration
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+ # subscription_id = var.subscription_id
+  #client_id       = var.client_id
+ # client_secret   = var.client_secret
+ # tenant_id       = var.tenant_id
 }
 
 #create resource group
